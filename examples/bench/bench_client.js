f02fbc5bf30bbaec360bd0c7665929e4c0c6c43e
Arlo <arlo.liu@gmail.com>
2019-06-11 22:16:57 +0800

    Add small packet request/response benchmark

--- examples/bench/bench_client.js
+++ examples/bench/bench_client.js
@@ -60,6 +60,32 @@ const bench2 = new ParallelBenchmark('test_cmd_raw_1k', {
     },
 });
 
-bench1.run().then(() => {
-    bench2.run();
+const bench3 = new ParallelBenchmark('test_cmd_raw_64kb', {
+    parallel: Parallel,
+    requests: Requests,
+    setup: function() {
+        this.rawData = Buffer.allocUnsafe(64);
+        return FastMQ.Client.connect('client2', 'master').then((ch) => {
+            this.channel = ch;
+        });
+    },
+    fn: function() {
+        // console.log(`this.number: ${this.number}`);
+        // let reqData = {num: this.number++, data: 'test'};
+        return this.channel.request('master', 'test_cmd_raw', this.rawData, 'raw');
+    },
+    verify: function(data) {
+        // console.log(data);
+    },
+    onStart: function(target) {
+        console.log(`# Run ${target.name}, parallel: ${target.parallel} requests: ${target.requests}`);
+    },
+    onComplete: function(stat) {
+        console.log(`# Benchmark ${stat.name}: ${stat.ops} operation per sec., elapsed: ${stat.elapsed} ms`);
+        this.channel.disconnect();
+    },
 });
+
+bench1.run()
+.then(() => bench2.run())
+.then(() => bench3.run());
